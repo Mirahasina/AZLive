63a04d83c0e761b193025cc0265ba49878c211c2
🤍️  Mirahasina  🤍️ <lovaramahandrimalala@gmail.com>
2026-06-18 11:16:54 +0300

    lova3

--- AZLive/AZLive/settings.py
+++ AZLive/AZLive/settings.py
@@ -213,7 +213,7 @@ TIKTOK_CLIENT_KEY = os.environ.get('TIKTOK_CLIENT_KEY', '')
 TIKTOK_CLIENT_SECRET = os.environ.get('TIKTOK_CLIENT_SECRET', '')
 TIKTOK_REDIRECT_URI = os.environ.get(
     'TIKTOK_REDIRECT_URI',
-    'https://limacine-adrian-sighted.ngrok-free.dev/api/auth/tiktok/callback/',
+    'http://localhost:8000/api/auth/facebook/callback/',
 )
 TIKTOK_LOGIN_SUCCESS_URL = os.environ.get(
     'TIKTOK_LOGIN_SUCCESS_URL',
