7ea45e80e332db8cf0ff7ba5592ed007b40dd7ca
🤍️  Mirahasina  🤍️ <lovaramahandrimalala@gmail.com>
2026-08-21 07:12:22 +0300

    verification

--- AZLive/AZLive/settings.py
+++ AZLive/AZLive/settings.py
@@ -277,7 +277,7 @@ TIKTOK_CLIENT_KEY = os.environ.get('TIKTOK_CLIENT_KEY', '')
 TIKTOK_CLIENT_SECRET = os.environ.get('TIKTOK_CLIENT_SECRET', '')
 TIKTOK_REDIRECT_URI = os.environ.get(
     'TIKTOK_REDIRECT_URI',
-    'http://localhost:8000/api/auth/facebook/callback/',
+    'http://localhost:8000/api/auth/tiktok/callback/',
 )
 TIKTOK_LOGIN_SUCCESS_URL = os.environ.get(
     'TIKTOK_LOGIN_SUCCESS_URL',
